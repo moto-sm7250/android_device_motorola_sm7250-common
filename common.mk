dd28913ddba45d47c6a0f469fb7c94330f7eef06
Albert Tang <tangalbert919@yahoo.com>
2024-08-20 08:14:22 +0530

    sm7250-common: Import task_profiles.json
    Change-Id: I922e83abeb47706303f2b83698ea07c09a65b069

--- common.mk
+++ common.mk
@@ -319,6 +319,13 @@ PRODUCT_PACKAGES += \
     libOmxVidcCommon \
     libstagefrighthw
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
